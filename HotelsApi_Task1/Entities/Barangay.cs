c0c21691b16cdca9d5e73a2b1625e8c563892607
Geraldlng <122422907+Geraldlng@users.noreply.github.com>
2025-04-03 12:24:13 +0800

    Add files via upload

---
+++ HotelsApi_Task1/Entities/Barangay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hotelApi.Entities;
+
+public partial class Barangay
+{
+    public int BarangayId { get; set; }
+
+    public string BarangayCode { get; set; } = null!;
+
+    public string BarangayName { get; set; } = null!;
+}
---
+++ HotelsApi_Task1/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hotelApi.Entities;
+
+public partial class City
+{
+    public int CityId { get; set; }
+
+    public string CityCode { get; set; } = null!;
+
+    public string CityName { get; set; } = null!;
+}
