966b0f921e8e4595b97978ce0a256263b66285a0
Geraldlng <122422907+Geraldlng@users.noreply.github.com>
2025-04-03 13:04:12 +0800

    Add files via upload

---
+++ HotelsApi_Task1/Entities/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hotelApi.Entities;
+
+public partial class State
+{
+    public int StateId { get; set; }
+
+    public string StateCode { get; set; } = null!;
+
+    public string StateName { get; set; } = null!;
+}
